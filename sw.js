ed9d3684f6362dfad17eaf6e46a3ad6e39390a31
Lamari <metlamari@gmail.com>
2026-08-28 15:45:38 +0300

    Bump SW cache to v0.2.6 to evict old app.js on existing clients

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // i-talk.gr service worker
 // Bump CACHE_VERSION when shell assets change so old caches are evicted.
-const CACHE_VERSION = 'italk-v0.2.5';
+const CACHE_VERSION = 'italk-v0.2.6';
 
 const SHELL_ASSETS = [
   '/',
